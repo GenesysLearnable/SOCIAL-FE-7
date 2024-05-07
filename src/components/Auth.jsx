b8f5a273801c4ea55f367e35143756302d042e36
Niz46 <paujohn66@gmamil.com>
2024-05-07 10:02:23 +0100

    team-led-commit

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaGoogle, FaFacebookF, FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const Auth = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleRegisterClick = () => {
+    setIsActive(true)
+  };
+
+  const handleLoginClick = () => {
+    setIsActive(false);
+  };
+
   return (
-    <div className="container" id="container">
+    <div className={`container ${isActive ? "active" : ""}`} id="container">
       <div className="form-container sign-up">
         <form>
           <h1>Create Account</h1>
           <div className="social-icons">
-            <a href="#" className="icon"><i className="fab fa-google-plus-g"></i></a>
-            <a href="#" className="icon"><i className="fab fa-facebook-f"></i></a>
-            <a href="#" className="icon"><i className="fab fa-github"></i></a>
-            <a href="#" className="icon"><i className="fab fa-linkedin-in"></i></a>
+            <a href="#" className="icon"><FaGoogle /></a>
+            <a href="#" className="icon"><FaFacebookF /></a>
+            <a href="#" className="icon"><FaGithub /></a>
+            <a href="#" className="icon"><FaLinkedinIn /></a>
           </div>
           <span>or use your email for registration</span>
           <input type="text" placeholder="Name" />
           <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
+          <input type="password" placeholder={"Password"} />
           <button>Sign Up</button>
         </form>
       </div>
@@ -23,10 +34,10 @@ const Auth = () => {
         <form>
           <h1>Sign In</h1>
           <div className="social-icons">
-            <a href="#" className="icon"><i className="fab fa-google-plus-g"></i></a>
-            <a href="#" className="icon"><i className="fab fa-facebook-f"></i></a>
-            <a href="#" className="icon"><i className="fab fa-github"></i></a>
-            <a href="#" className="icon"><i className="fab fa-linkedin-in"></i></a>
+            <a href="#" className="icon"><FaGoogle /></a>
+            <a href="#" className="icon"><FaFacebookF /></a>
+            <a href="#" className="icon"><FaGithub /></a>
+            <a href="#" className="icon"><FaLinkedinIn /></a>
           </div>
           <span>or use your email and password</span>
           <input type="email" placeholder="Email" />
@@ -40,12 +51,12 @@ const Auth = () => {
           <div className="toggle-panel toggle-left">
             <h1>Welcome Back!</h1>
             <p>Enter your personal details to use all site features</p>
-            <button className="hidden" id="login">Sign In</button>
+            <button className="hidden" id="login" onClick={handleLoginClick}>Sign In</button>
           </div>
           <div className="toggle-panel toggle-right">
             <h1>Hello, Friend!</h1>
             <p>Register with your personal details to use all site features</p>
-            <button className="hidden" id="register">Sign Up</button>
+            <button className="hidden" id="register" onClick={handleRegisterClick}>Sign Up</button>
           </div>
         </div>
       </div>
