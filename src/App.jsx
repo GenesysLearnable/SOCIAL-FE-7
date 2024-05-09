c88be18353696d881fa29671d07e944142f56e0b
fx-dove <opiechisomsamuel@gmail.com>
2024-05-09 18:26:39 +0200

    opie

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import ProfileSetup from "./components/ProfileSetup.jsx";
 import Auth from "./components/Auth.jsx";
 
 function App() {
   return (
     <>
-      <Auth />
+      
+      <Auth/>
     </>
   );
 }
