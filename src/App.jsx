269ae5399703c70848b8c7eb10c447fb954b237c
Niz46 <paujohn66@gmail.com>
2024-05-11 13:57:30 +0100

    structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,42 @@
 import "./App.css";
-import { createBrowserRouter,RouterProvider,Outlet,Navigate } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  // Outlet,
+  // Navigate,
+} from "react-router-dom";
 import ProfileSetup from "./components/ProfileSetup.jsx";
 import AddFriends from "./components/AddFriend.jsx";
 import Auth from "./components/Auth.jsx";
 // import MetaMask from "./components/Transcation.jsx";
 
 function App() {
-
   const router = createBrowserRouter([
     {
-      path: '/',
-      element:<Auth/>
+      path: "/",
+      element: <Auth />,
     },
     {
-      path:'/setup',
-      element:<ProfileSetup/>
+      path: "/setup",
+      element: <ProfileSetup />,
     },
     {
-      path:'/addFriends',
-      element:<AddFriends/>
-    }
-    
+      path: "/addFriends",
+      element: <AddFriends />,
+    },
   ]);
   return (
+<<<<<<< HEAD
    <>
     <RouterProvider router={router}/>
    </>
 
+=======
+    <>
+      <RouterProvider router={router} />
+    </>
+>>>>>>> 45aed4a (structure)
   );
 }
 
 export default App;
-
