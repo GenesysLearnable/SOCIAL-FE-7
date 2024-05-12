85d012c9b469a33b6b75ecbbeb423bab3ecd2d28
BRChris123 <ihemkwaba@gmail.com>
2024-05-12 12:08:27 +0100

    christopher call commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import "./App.css";
+import Call from "./components/Call";
 // import AddFriends from "./components/addfriend/AddFriend.jsx";
-import Auth from "./components/Auth.jsx";
+// import Auth from "./components/Auth.jsx";
 // import MetaMask from "./components/Transcation.jsx";
 
 function App() {
   return (
     <>
       {/* <ProfileSetup/> */}
-      <Auth />
+      {/* <Auth /> */}
       {/* <AddFriends /> */}
       {/* <MetaMask /> */}
+      <Call />
     </>
   );
 }
 
 export default App;
-
